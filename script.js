67a2acf3e339b1e0e255291e34d9b524309b3454
Silvija <silvija.prozinger@gmail.com>
2019-06-02 18:18:24 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+/* toggle about menu slide text */
+
+$('#slide').click(function(){
+	$('#slide-txt').slideToggle(2000);
+});
+
+/* toggle the menu items */
+
+$('#hamburger-menu').click(function(){
+	$('#menu-items').slideToggle(1000);
+});
