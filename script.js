2c3ce4a496a890f6aed48b4165628e30ca7755d2
Silvija <silvija.prozinger@gmail.com>
2019-06-02 21:26:25 +0200

    deleted some comments

--- script.js
+++ script.js
@@ -9,21 +9,4 @@ $('#slide').click(function(){
 $('#hamburger-menu').click(function(){
 	$('#menu-items').slideToggle(1000);
 });
-
-/*
-$(document).ready(function() {
-    // run test on initial page load
-    checkSize();
-
-    // run test on resize of the window
-    $(window).resize(checkSize);
-});
-
-//Function to the css rule
-function checkSize(){
-    ($(window).width() <= 621)
-    	$('#slide').click(function(){
-			$('#slide-txt').slideToggle({direction: 'right'}, 2000);
-		});
-    }
-*/    
+   
